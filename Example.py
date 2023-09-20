f734131dcddf7f8b4b4d17a4a0ec910be88da45a
Afonso Azenha <76539773+kaiuki2000@users.noreply.github.com>
2023-09-20 20:18:30 +0100

    Added Example.py

---
+++ Example.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use('https://github.com/kaiuki2000/PitayaRemix/raw/main/MyStyle.mplstyle')
+
+x = np.linspace(0, 10, 1000)
+plt.plot(x, np.sin(x), label = r'$\sin(x)$')
+plt.plot(x, np.cos(x), label = r'$\cos(x)$')
+plt.plot(x, np.tanh(x), label = r'$\tanh(x)$')
+plt.title('Trigonometric functions')
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+plt.legend()
+plt.show()
